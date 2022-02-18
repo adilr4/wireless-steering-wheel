f0f377628b369ffbaeeddef63e2644ed8970ea13
amila-sinanovic <98643373+amila-sinanovic@users.noreply.github.com>
2022-02-18 12:31:24 +0100

    sending commands (#3)

--- steeringWheel/main.c
+++ steeringWheel/main.c
@@ -1,9 +1,16 @@
 #include "lis302dl.h"
+#include "usart.h"
+
+void init() {
+  initAccelerometer();
+  initUSART2(USART2_BAUDRATE_9600);
+  enIrqUSART2();
+}
 
 int main(void) { 
+  init();
+
   int8_t accelerometerData[3];
-  
-  initAccelerometer(); 
   
   getDataFromAccelerometer(accelerometerData);
  
---
+++ steeringWheel/usart.h
@@ -0,0 +1,25 @@
+#ifndef __USART2_H_
+#define __USART2_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+#include "stm32f4xx.h"
+
+#define USART2_BUFFER_SIZE 512
+
+#define USART2_BAUDRATE_921600 0x0000002D
+#define USART2_BAUDRATE_460800 0x0000005B
+#define USART2_BAUDRATE_115200 0x0000016C
+#define USART2_BAUDRATE_9600 0x00001117
+
+void initUSART2(uint32_t baudrate);
+void putcharUSART2(uint8_t data);
+void sendCommand(uint16_t command);
+
+void enIrqUSART2(void);
+void disIrqUSART2(void);
+
+extern volatile uint8_t g_commandsBuffer[USART2_BUFFER_SIZE];
+extern volatile uint16_t g_usart2_widx;
+extern volatile uint16_t g_usart2_ridx;
+#endif

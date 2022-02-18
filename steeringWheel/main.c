dd780baa4d8acfbe4f92dadacdcb6e41a451a1bd
maid.ostrakovic <maid.ostrakovic@fet.ba>
2022-02-18 13:23:10 +0000

    prepare command to send

--- steeringWheel/main.c
+++ steeringWheel/main.c
@@ -1,7 +1,17 @@
+#include <math.h>
+#include "delay.h"
 #include "lis302dl.h"
 #include "usart.h"
 
+void getDataFromAngle(float, float);
+void scaleWithPotenciometer();
+uint16_t createCommand();
+
 const int angleOffset = 9;
+int8_t leftWheel;
+int8_t rightWheel;
+uint8_t directionMode = 1;
+uint8_t stopMode = 0;
 
 void init() {
   initAccelerometer();
@@ -9,43 +19,79 @@ void init() {
   enIrqUSART2();
 }
 
-int main(void) { 
+int main(void) {
   init();
 
   int8_t accelerometerData[3];
-  
-  getDataFromAccelerometer(accelerometerData);
- 
-  //TODO prepare accelerometer data to send via bluetooth
 
-  return 0; 
+  while (1) {
+    getDataFromAccelerometer(accelerometerData);
+
+    float x = accelerometerData[0];
+    float y = accelerometerData[1];
+    float z = accelerometerData[2];
+
+    float xAngle = atanf(x / sqrt(y * y + z * z)) * 180 / 3.14;
+    float yAngle = atanf(y / sqrt(x * x + z * z)) * 180 / 3.14;
+
+    getDataFromAngle(xAngle, yAngle);
+
+    // scaleWithPotenciometer();
+
+    sendCommand(createCommand());
+
+    delay_ms(1000);
+  }
+  return 0;
+}
+
+uint16_t createCommand() {
+  uint8_t header = 0x0C;
+  if (stopMode) {
+    header |= 0x02;
+  }
+  if (directionMode) {
+    header |= 0x01;
+  }
+
+  uint8_t high = (leftWheel & 0x0f);
+  uint8_t low = (rightWheel & 0x0f);
+
+  uint8_t checksum = 0x0f - ((header + high + low) & 0x0f);
+
+  uint16_t command = 0x0;
+  command |= (header << 12) | (high << 8) | (low << 4) | checksum;
+
+  return command;
 }
 
 void getDataFromAngle(float x, float y) {
-	if(x > 0) {	//ispravan polozaj
-		for (int i = 0; i < 10; ++i)
-			if(y < -81 + i * angleOffset) {
-				l = i;
-				r = 9;
-				return;
-			}
+  if (x > 0) {  // ispravan polozaj
+    for (int i = 0; i < 10; ++i)
+      if (y < -81 + i * angleOffset) {
+        leftWheel = i;
+        rightWheel = 9;
+        return;
+      }
 
-		for (int i = 0; i < 10; ++i)
-			if(y < 9 + i * angleOffset) {
-				l = 9;
-				r = 9 - i;
-				return;
-			}
-	} else {//neispravan polozaj
-		if(y > 70) {
-			l = 9;
-			r = 0;
-		} else if(y < -70) {
-			l = 0;
-			r = 9;
-		} else {
-			//stani
-		}
-	}
+    for (int i = 0; i < 10; ++i)
+      if (y < 9 + i * angleOffset) {
+        leftWheel = 9;
+        rightWheel = 9 - i;
+        return;
+      }
+  } else {  // neispravan polozaj
+    if (y > 70) {
+      leftWheel = 9;
+      rightWheel = 0;
+    } else if (y < -70) {
+      leftWheel = 0;
+      rightWheel = 9;
+    } else {
+      // stani
+    }
+  }
 }
+
+void scaleWithPotenciometer() {}
 

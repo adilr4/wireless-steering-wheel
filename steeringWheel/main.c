08ab9e5e5e7f8254e622b665f578fb919ae4c297
Amila <amila.sinanovic@fet.ba>
2022-02-18 13:45:33 +0100

    getting data from angles

--- steeringWheel/main.c
+++ steeringWheel/main.c
@@ -1,6 +1,8 @@
 #include "lis302dl.h"
 #include "usart.h"
 
+const int angleOffset = 9;
+
 void init() {
   initAccelerometer();
   initUSART2(USART2_BAUDRATE_9600);
@@ -18,3 +20,32 @@ int main(void) {
 
   return 0; 
 }
+
+void getDataFromAngle(float x, float y) {
+	if(x > 0) {	//ispravan polozaj
+		for (int i = 0; i < 10; ++i)
+			if(y < -81 + i * angleOffset) {
+				l = i;
+				r = 9;
+				return;
+			}
+
+		for (int i = 0; i < 10; ++i)
+			if(y < 9 + i * angleOffset) {
+				l = 9;
+				r = 9 - i;
+				return;
+			}
+	} else {//neispravan polozaj
+		if(y > 70) {
+			l = 9;
+			r = 0;
+		} else if(y < -70) {
+			l = 0;
+			r = 9;
+		} else {
+			//stani
+		}
+	}
+}
+
